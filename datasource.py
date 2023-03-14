79ce167675ce5d01770ac13ec08ca246ffd711a4
Phil Tsaryk <phil@tsaryk.com>
2023-03-14 22:34:35 +0100

    WIP

--- datasource.py
+++ datasource.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2021 PeppyMeter peppy.player@gmail.com
-# 
+#
 # This file is part of PeppyMeter.
-# 
+#
 # PeppyMeter is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # PeppyMeter is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with PeppyMeter. If not, see <http://www.gnu.org/licenses/>.
 
@@ -41,14 +41,15 @@ STEREO_ALGORITHM_NEW = "new"
 STEREO_ALGORITHM_LOGARITHM = "logarithm"
 STEREO_ALGORITHM_AVERAGE = "average"
 
+
 class DataSource(object):
-    """ Provides methods to generate different types of audio signal. """
-    
+    """Provides methods to generate different types of audio signal."""
+
     lock = RLock()
-    
+
     def __init__(self, util):
-        """ Initializer
-        
+        """Initializer
+
         :param c: configuration dictionary
         """
         self.volume = 1
@@ -61,11 +62,11 @@ class DataSource(object):
         self.min = self.config[VOLUME_MIN]
         self.max_in_ui = self.config[VOLUME_MAX]
         self.max_in_pipe = self.config[VOLUME_MAX_IN_PIPE]
-        
+
         self.v = 0
         self.step = self.config[STEP]
         self.pipe_size = 4
-        self.PIPE_BUFFER_SIZE = 1048576 # as defined for Raspberry OS in /proc/sys/fs/pipe-max-size
+        self.PIPE_BUFFER_SIZE = 1048576  # as defined for Raspberry OS in /proc/sys/fs/pipe-max-size
         self.rng = list(range(int(self.min), int(self.max_in_ui)))
         self.double_rng = self.rng
         self.double_rng.extend(range(int(self.max_in_ui) - 1, int(self.min), -1))
@@ -81,9 +82,9 @@ class DataSource(object):
         self.data = ()
         self.http_data = ()
         self.smooth_buffer_size = self.config[SMOOTH_BUFFER_SIZE]
-        self.smooth_buffer = deque(self.smooth_buffer_size*[0], self.smooth_buffer_size)
+        self.smooth_buffer = deque(self.smooth_buffer_size * [0], self.smooth_buffer_size)
         for _ in range(self.smooth_buffer_size):
-            self.smooth_buffer.append((0,0,0))
+            self.smooth_buffer.append((0, 0, 0))
 
         self.SOURCE_FUNCTIONS = {
             SOURCE_CONSTANT: self.get_constant_value,
@@ -92,13 +93,13 @@ class DataSource(object):
             SOURCE_TRIANGLE: self.get_triangle_value,
             SOURCE_SINE: self.get_sine_value,
             SOURCE_PIPE: self.get_pipe_value,
-            SOURCE_HTTP: self.get_http_value
+            SOURCE_HTTP: self.get_http_value,
         }
-    
-    def open_pipe(self):
-        """ Open named pipe """
 
-        try:            
+    def open_pipe(self):
+        """Open named pipe"""
+
+        try:
             logging.debug("opening pipe...")
             self.pipe = os.open(self.pipe_name, os.O_RDONLY | os.O_NONBLOCK)
             logging.debug("pipe opened")
@@ -106,7 +107,7 @@ class DataSource(object):
             logging.debug("Cannot open named pipe: " + self.pipe_name)
 
     def flush_pipe_buffer(self):
-        """ Flush data from the pipe """
+        """Flush data from the pipe"""
 
         if not self.pipe:
             return
@@ -119,7 +120,7 @@ class DataSource(object):
         logging.debug("pipe flushed")
 
     def start_data_source(self):
-        """ Start data source thread. """ 
+        """Start data source thread."""
 
         logging.debug("starting data source...")
 
@@ -131,70 +132,70 @@ class DataSource(object):
         thread.start()
 
         logging.debug("data source started")
-        
+
     def stop_data_source(self):
-        """ Stop data source thread. """ 
-               
+        """Stop data source thread."""
+
         self.run_flag = False
-    
+
     def get_current_data(self):
-        """ Return current data """
-        
+        """Return current data"""
+
         with self.lock:
             return self.data
-        
+
     def get_current_left_channel_data(self):
-        """ Return current left channel value """
-        
+        """Return current left channel value"""
+
         with self.lock:
             if self.data and self.data[0]:
                 return self.data[0]
             else:
                 return None
-    
+
     def get_current_right_channel_data(self):
-        """ Return current right channel value """
-        
+        """Return current right channel value"""
+
         with self.lock:
             if self.data and self.data[1]:
                 return self.data[1]
             else:
                 return None
-        
+
     def get_current_mono_channel_data(self):
-        """ Return current mono value """
+        """Return current mono value"""
 
         with self.lock:
             if self.data and self.data[2]:
                 return self.data[2]
             else:
                 return None
-    
+
     def get_data(self):
-        """ Thread method. """ 
-               
+        """Thread method."""
+
         while self.run_flag:
             with self.lock:
                 self.data = self.get_value()
             time.sleep(self.polling_interval)
-    
+
     def get_value(self):
-        """ Get data depending on the data source type. """ 
-               
+        """Get data depending on the data source type."""
+
         return self.SOURCE_FUNCTIONS[self.ds_type]()
-    
+
     def get_constant_value(self):
-        """ Returns constant value for all channels. The value is defined in the configuration file. """ 
-               
+        """Returns constant value for all channels. The value is defined in the configuration file."""
+
         return (self.const, self.const, self.const)
-    
+
     def get_noise_value(self):
-        """ Generate random value for all channels. """
-        
+        """Generate random value for all channels."""
+
         new_left = uniform(self.min, self.max_in_ui)
-        new_right = uniform(self.min, self.max_in_ui)            
+        new_right = uniform(self.min, self.max_in_ui)
         new_mono = self.get_mono(new_left, new_right)
-        
+
         left = self.get_channel(self.previous_left, new_left)
         right = self.get_channel(self.previous_right, new_right)
         mono = self.get_channel(self.previous_mono, new_mono)
@@ -204,15 +205,15 @@ class DataSource(object):
             left = self.get_smooth_value(0)
             right = self.get_smooth_value(1)
             mono = self.get_smooth_value(2)
-        
+
         self.previous_left = new_left
         self.previous_right = new_right
         self.previous_mono = new_mono
 
         return (left, right, mono)
-    
+
     def get_smooth_value(self, index):
-        """ Get smooth value
+        """Get smooth value
 
         :param index: channel index
         :return: smooth value calculated using buffer
@@ -223,31 +224,31 @@ class DataSource(object):
         return s / self.smooth_buffer_size
 
     def get_saw_value(self):
-        """ Generate saw shape signal. """ 
-        
-        value = self.rng[int(self.v)]      
+        """Generate saw shape signal."""
+
+        value = self.rng[int(self.v)]
         s = (value, value, value)
         self.v = (self.v + self.step) % self.max_in_ui
         return s
-    
+
     def get_triangle_value(self):
-        """ Generate triangle shape signal. """
-        
+        """Generate triangle shape signal."""
+
         value = self.double_rng[self.v]
         t = (value, value, value)
         self.v = (self.v + self.step) % (int(self.max_in_ui * 2 - 1))
         return t
-    
+
     def get_sine_value(self):
-        """ Generate sine shape signal. """ 
-               
-        a = int(self.max_in_ui * ((1 + math.sin(math.radians(-90 + self.v)))/2))
+        """Generate sine shape signal."""
+
+        a = int(self.max_in_ui * ((1 + math.sin(math.radians(-90 + self.v))) / 2))
         s = (a, a, a)
         self.v = (self.v + self.step * 6) % 360
         return s
 
     def get_latest_pipe_data(self):
-        """ Read from the named pipe until it's empty """
+        """Read from the named pipe until it's empty"""
 
         latest_data = [0, 0, 0, 0]
         data = None
@@ -264,13 +265,13 @@ class DataSource(object):
         return latest_data
 
     def get_http_value(self):
-        """ Fetch HTTP value """
+        """Fetch HTTP value"""
 
         with self.lock:
             return self.http_data
 
     def get_pipe_value(self):
-        """ Get signal from the named pipe. """
+        """Get signal from the named pipe."""
 
         data = None
         left = right = mono = 0.0
@@ -283,14 +284,14 @@ class DataSource(object):
 
         try:
             data = self.get_latest_pipe_data()
-            length = len(data) 
+            length = len(data)
             if length == 0:
                 return (0, 0, 0)
-            
-            new_left = int(self.max_in_ui * ((data[length - 4] + (data[length - 3] << 8)) / self.max_in_pipe))
-            new_right = int(self.max_in_ui * ((data[length - 2] + (data[length - 1] << 8)) / self.max_in_pipe))
+
+            new_left = int(self.max_in_ui * ((data[length - 4] + (data[length - 3] << 8)) / volume_level))
+            new_right = int(self.max_in_ui * ((data[length - 2] + (data[length - 1] << 8)) / volume_level))
             new_mono = self.get_mono(new_left, new_right)
-            
+
             left = self.get_channel(self.previous_left, new_left)
             right = self.get_channel(self.previous_right, new_right)
             mono = self.get_channel(self.previous_mono, new_mono)
@@ -300,40 +301,40 @@ class DataSource(object):
                 left = self.get_smooth_value(0)
                 right = self.get_smooth_value(1)
                 mono = self.get_smooth_value(2)
-            
+
             self.previous_left = left
             self.previous_right = right
             self.previous_mono = mono
         except Exception as e:
             logging.debug(e)
-        
+
         return (self.previous_left, self.previous_right, self.previous_mono)
-    
+
     def get_mono(self, left, right):
-        """ Create mono signal from stereo using particular algorithm """ 
-                      
+        """Create mono signal from stereo using particular algorithm"""
+
         if self.mono_algorithm == MONO_ALGORITHM_MAXIMUM:
             mono = max(left, right)
         elif self.mono_algorithm == MONO_ALGORITHM_AVERAGE:
             mono = statistics.mean([left, right])
         return mono
-    
+
     def get_channel(self, previous_value, new_value):
-        """ Prepares signal value depending on the previous one and algorithm. """ 
-               
+        """Prepares signal value depending on the previous one and algorithm."""
+
         if self.stereo_algorithm == STEREO_ALGORITHM_NEW:
             channel_value = new_value
         elif self.stereo_algorithm == STEREO_ALGORITHM_LOGARITHM:
             if previous_value == 0.0:
                 channel_value = 0.0
             else:
-                channel_value = 20 * math.log10(new_value/previous_value)
+                channel_value = 20 * math.log10(new_value / previous_value)
             if channel_value < -20:
                 channel_value = -20
             if channel_value > 3:
                 channel_value = 3
-            channel_value = (channel_value + 20) * (100/23)
+            channel_value = (channel_value + 20) * (100 / 23)
         elif self.stereo_algorithm == STEREO_ALGORITHM_AVERAGE:
             channel_value = statistics.mean([previous_value, new_value])
-                
+
         return channel_value
--- peppymeter.py
+++ peppymeter.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2021 PeppyMeter peppy.player@gmail.com
-# 
+#
 # This file is part of PeppyMeter.
-# 
+#
 # PeppyMeter is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # PeppyMeter is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with PeppyMeter. If not, see <http://www.gnu.org/licenses/>.
 
@@ -31,12 +31,13 @@ from httpinterface import HTTPInterface
 from screensavermeter import ScreensaverMeter
 from configfileparser import *
 
+
 class Peppymeter(ScreensaverMeter):
-    """ Peppy Meter class """
-    
+    """Peppy Meter class"""
+
     def __init__(self, util=None, standalone=False):
-        """ Initializer
-        
+        """Initializer
+
         :param util: utility object
         :param standalone: True - standalone version, False - part of Peppy player
         """
@@ -45,9 +46,9 @@ class Peppymeter(ScreensaverMeter):
             self.util = util
         else:
             self.util = MeterUtil()
-            
+
         self.use_vu_meter = getattr(self.util, USE_VU_METER, None)
-        
+
         self.name = "peppymeter"
 
         base_path = "."
@@ -55,45 +56,43 @@ class Peppymeter(ScreensaverMeter):
             pkg_parts = __package__.split(".")
             if len(pkg_parts) > 0:
                 base_path = os.path.join(os.getcwd(), "screensaver", self.name)
-        
+
         parser = ConfigFileParser(base_path)
         self.util.meter_config = parser.meter_config
         self.util.exit_function = self.exit
         self.outputs = {}
-        
+
         if standalone:
             if self.util.meter_config[USE_LOGGING]:
                 log_handlers = []
                 try:
                     log_handlers.append(logging.StreamHandler(sys.stdout))
-                    log_handlers.append(logging.FileHandler(filename="peppymeter.log", mode='w'))
+                    log_handlers.append(logging.FileHandler(filename="peppymeter.log", mode="w"))
                     logging.basicConfig(
-                        level=logging.NOTSET,
-                        format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
-                        handlers=log_handlers
+                        level=logging.NOTSET, format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s", handlers=log_handlers
                     )
                 except:
                     pass
             else:
                 logging.disable(logging.CRITICAL)
-        
+
         # no VU Meter support for Windows
         if "win" in sys.platform and self.util.meter_config[DATA_SOURCE][TYPE] == SOURCE_PIPE:
             self.util.meter_config[DATA_SOURCE][TYPE] = SOURCE_NOISE
-        
+
         self.data_source = DataSource(self.util.meter_config)
         if self.util.meter_config[DATA_SOURCE][TYPE] == SOURCE_PIPE or self.use_vu_meter == True:
             self.data_source.start_data_source()
-        
+
         if self.util.meter_config[OUTPUT_DISPLAY]:
             self.meter = self.output_display(self.data_source)
-            
+
         if self.util.meter_config[OUTPUT_SERIAL]:
             self.outputs[OUTPUT_SERIAL] = SerialInterface(self.util.meter_config, self.data_source)
-            
+
         if self.util.meter_config[OUTPUT_I2C]:
             self.outputs[OUTPUT_I2C] = I2CInterface(self.util.meter_config, self.data_source)
-            
+
         if self.util.meter_config[OUTPUT_PWM]:
             self.outputs[OUTPUT_PWM] = PWMInterface(self.util.meter_config, self.data_source)
 
@@ -102,26 +101,26 @@ class Peppymeter(ScreensaverMeter):
 
         self.start_interface_outputs()
         logging.debug("PeppyMeter initialized")
-    
+
     def output_display(self, data_source):
-        """ Initialize display
-        
+        """Initialize display
+
         :data_source: data source
         :return: graphical VU Meter
         """
-        meter = Vumeter(self.util, data_source)         
+        meter = Vumeter(self.util, data_source)
         self.current_image = None
         self.update_period = meter.get_update_period()
-        
+
         return meter
-    
+
     def init_display(self):
-        """ Initialize Pygame display """
+        """Initialize Pygame display"""
 
         screen_w = self.util.meter_config[SCREEN_INFO][WIDTH]
         screen_h = self.util.meter_config[SCREEN_INFO][HEIGHT]
         depth = self.util.meter_config[SCREEN_INFO][DEPTH]
-        
+
         os.environ["SDL_FBDEV"] = self.util.meter_config[SDL_ENV][FRAMEBUFFER_DEVICE]
 
         if self.util.meter_config[SDL_ENV][MOUSE_ENABLED]:
@@ -129,25 +128,25 @@ class Peppymeter(ScreensaverMeter):
             os.environ["SDL_MOUSEDRV"] = self.util.meter_config[SDL_ENV][MOUSE_DRIVER]
         else:
             os.environ["SDL_NOMOUSE"] = "1"
-        
+
         if not self.util.meter_config[OUTPUT_DISPLAY]:
             os.environ["SDL_VIDEODRIVER"] = self.util.meter_config[SDL_ENV][VIDEO_DRIVER]
             os.environ["DISPLAY"] = self.util.meter_config[SDL_ENV][VIDEO_DISPLAY]
             pygame.display.init()
             pygame.font.init()
             if self.util.meter_config[SDL_ENV][DOUBLE_BUFFER]:
-                self.util.PYGAME_SCREEN = pygame.display.set_mode((1,1), pygame.DOUBLEBUF, depth)
+                self.util.PYGAME_SCREEN = pygame.display.set_mode((1, 1), pygame.DOUBLEBUF, depth)
             else:
-                self.util.PYGAME_SCREEN = pygame.display.set_mode((1,1))
+                self.util.PYGAME_SCREEN = pygame.display.set_mode((1, 1))
             return
-        
+
         if "win" not in sys.platform:
             if not self.util.meter_config[SDL_ENV][VIDEO_DRIVER] == "dummy":
                 os.environ["SDL_VIDEODRIVER"] = self.util.meter_config[SDL_ENV][VIDEO_DRIVER]
                 os.environ["DISPLAY"] = self.util.meter_config[SDL_ENV][VIDEO_DISPLAY]
             pygame.display.init()
             pygame.mouse.set_visible(False)
-        else:            
+        else:
             pygame.init()
             pygame.display.set_caption("Peppy Meter")
 
@@ -164,19 +163,19 @@ class Peppymeter(ScreensaverMeter):
             else:
                 self.util.PYGAME_SCREEN = pygame.display.set_mode((screen_w, screen_h))
 
-        self.util.meter_config[SCREEN_RECT] = pygame.Rect(0, 0, screen_w, screen_h) 
-    
+        self.util.meter_config[SCREEN_RECT] = pygame.Rect(0, 0, screen_w, screen_h)
+
     def start_interface_outputs(self):
-        """ Starts writing interfaces """
+        """Starts writing interfaces"""
 
         if self.util.meter_config[OUTPUT_SERIAL]:
             self.serial_interface = self.outputs[OUTPUT_SERIAL]
             self.serial_interface.start_writing()
-        
+
         if self.util.meter_config[OUTPUT_I2C]:
             self.i2c_interface = self.outputs[OUTPUT_I2C]
             self.i2c_interface.start_writing()
-            
+
         if self.util.meter_config[OUTPUT_PWM]:
             self.pwm_interface = self.outputs[OUTPUT_PWM]
             self.pwm_interface.start_writing()
@@ -184,9 +183,9 @@ class Peppymeter(ScreensaverMeter):
         if self.util.meter_config[OUTPUT_HTTP]:
             self.http_interface = self.outputs[OUTPUT_HTTP]
             self.http_interface.start_writing()
-    
+
     def start(self):
-        """ Start VU meter. This method called by Peppy Meter to start meter """
+        """Start VU meter. This method called by Peppy Meter to start meter"""
 
         pygame.event.clear()
         if self.util.meter_config[DATA_SOURCE][TYPE] == SOURCE_PIPE or self.use_vu_meter == True:
@@ -196,10 +195,10 @@ class Peppymeter(ScreensaverMeter):
         if self.util.meter_config[OUTPUT_HTTP]:
             self.http_interface = self.outputs[OUTPUT_HTTP]
             self.http_interface.start_writing()
-        
+
     def start_display_output(self):
-        """ Start thread for graphical VU meter """
-        
+        """Start thread for graphical VU meter"""
+
         pygame.event.clear()
         clock = Clock()
         self.meter.start()
@@ -210,7 +209,7 @@ class Peppymeter(ScreensaverMeter):
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-                    keys = pygame.key.get_pressed() 
+                    keys = pygame.key.get_pressed()
                     if (keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]) and event.key == pygame.K_c:
                         running = False
                 elif event.type == pygame.MOUSEBUTTONUP and (self.util.meter_config[EXIT_ON_TOUCH] or self.util.meter_config[STOP_DISPLAY_ON_TOUCH]):
@@ -226,7 +225,7 @@ class Peppymeter(ScreensaverMeter):
             self.exit()
 
     def stop(self):
-        """ Stop meter animation. """
+        """Stop meter animation."""
 
         if not (self.util.meter_config[DATA_SOURCE][TYPE] == SOURCE_PIPE and self.use_vu_meter == True):
             self.data_source.stop_data_source()
@@ -235,22 +234,22 @@ class Peppymeter(ScreensaverMeter):
         if self.util.meter_config[OUTPUT_HTTP]:
             self.http_interface = self.outputs[OUTPUT_HTTP]
             self.http_interface.stop_writing()
-    
+
     def refresh(self):
-        """ Refresh meter. Used to switch from one random meter to another. """
-        
+        """Refresh meter. Used to switch from one random meter to another."""
+
         self.meter.refresh()
 
     def set_volume(self, volume):
-        """ Set volume level.
+        """Set volume level.
 
         :param volume: new volume level
         """
         self.data_source.volume = volume
-    
+
     def exit(self):
-        """ Exit program """
-        
+        """Exit program"""
+
         if self.util.meter_config[OUTPUT_SERIAL]:
             self.serial_interface.stop_writing()
         if self.util.meter_config[OUTPUT_I2C]:
@@ -267,30 +266,32 @@ class Peppymeter(ScreensaverMeter):
         os._exit(0)
 
     def set_visible(self, flag):
-        """ Set visible/invisible flag.
+        """Set visible/invisible flag.
 
         :param flag: True - visible, False - invisible
         """
         pass
-       
+
+
 if __name__ == "__main__":
-    """ This is called by stand-alone PeppyMeter """
+    """This is called by stand-alone PeppyMeter"""
     pm = Peppymeter(standalone=True)
     source = pm.util.meter_config[DATA_SOURCE][TYPE]
 
     if source == SOURCE_HTTP:
         try:
-            f = open(os.devnull, 'w')
+            f = open(os.devnull, "w")
             sys.stdout = sys.stderr = f
             from webserver import WebServer
+
             web_server = WebServer(pm)
         except Exception as e:
             logging.debug(e)
 
     if source != SOURCE_PIPE:
         pm.data_source.start_data_source()
-        
+
     pm.init_display()
-        
+
     if pm.util.meter_config[OUTPUT_DISPLAY]:
-        pm.start_display_output()    
+        pm.start_display_output()
