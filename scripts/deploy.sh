53e0ac70d82f5c3d24a4fa5429df46d43a47e463
Phil Tsaryk <phil@tsaryk.com>
2023-03-15 07:14:37 +0100

    chore: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,5 @@ CURRENT_COMMIT=$(git rev-parse HEAD)
 ssh volumio <<EOF
   cd /data/plugins/miscellanea/peppy_screensaver/peppymeter/
   git fetch
-  git checkout np-01-config
-  git stash
-  git rebase --onto ${CURRENT_COMMIT} HEAD~1 np-01-config
-  git stash pop
+  git checkout ${CURRENT_COMMIT}
 EOF
